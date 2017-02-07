845a7b1b13f8e5f9035042a741fc26b5b6907fec
Dan <email@danielhand.io>
2017-02-07 08:09:58 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php add_action( 'amp_post_template_css', 'speakwp_amp_additional_css_styles' );
 function speakwp_amp_additional_css_styles( $amp_template ) {
-    // only CSS here please...
-    ?>
-    <?php
-}
 ?>
+    // only CSS here please...
+<?php } ?>
